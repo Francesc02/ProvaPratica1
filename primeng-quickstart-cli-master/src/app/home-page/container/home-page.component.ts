05b8c80cfcd0d0971284353bcccafa13b290c5ce
francesco solito <francescosolitolav@gmail.com>
2024-02-15 10:35:53 +0100

    Revert "gestione search non da risultato"
    This reverts commit 6ef2a2c892bd4c969e8978396b2e21afe6f67b84.
    
    # Conflicts:
    #	primeng-quickstart-cli-master/src/app/header/header.component.html
    #	primeng-quickstart-cli-master/src/app/home-page/container/home-page.component.html
    #	primeng-quickstart-cli-master/src/app/home-page/container/home-page.component.ts

--- primeng-quickstart-cli-master/src/app/home-page/container/home-page.component.ts
+++ primeng-quickstart-cli-master/src/app/home-page/container/home-page.component.ts
@@ -2,43 +2,37 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CardModule } from 'primeng/card';
 import { ChiamateAPIService } from 'src/app/services/chiamate-api.service';
-import { PrimeIcons} from 'primeng/api';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css'],
+  styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-  film: unknown | null = null;
-  query: string | undefined;
 
-  constructor(
-    public chiamateApi: ChiamateAPIService,
-    public route: ActivatedRoute
-  ) {}
+  film: unknown | null = null;
+  query:string | undefined;
+
+  constructor(public chiamateApi:ChiamateAPIService,public route:ActivatedRoute){}
+
+
 
   ngOnInit(): void {
     this.query = JSON.stringify(this.route.snapshot.queryParams['query']);
-    this.route.params.subscribe((params) => {
+    this.route.params.subscribe(params => {
       this.query = params['query'];
-    });
-    console.log(this.query);
+  }); 
+  console.log(this.query);
 
-    if (this.query) {
-      this.chiamateApi.searchFilm(this.query).subscribe((result) => {
-        if (result.total_results == 0) {
-          this.film = 'Nessun film trovato, nome film non valido!';
-        } else {
-          this.film = result;
-        }
-        console.log(this.film);
-      });
-    } else {
-      this.chiamateApi.getFilm().subscribe((result) => {
-        this.film = result;
-        console.log(this.film);
-      });
-    }
-  }
+  if(this.query){
+    this.chiamateApi.searchFilm(this.query).subscribe(result=>{
+      this.film=result;
+    })
+  }else{
+    this.chiamateApi.getFilm().subscribe(result=>{
+      this.film=result;
+      console.log(this.film);
+    })
+  }  
+}
 }
